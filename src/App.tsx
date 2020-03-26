51bff325123aa858f98f02c1a1f12082959d5f55
kwansikdev <kwansk0424@gmail.com>
2020-03-27 00:06:54 +0900

    feat: Remove createGlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { createGlobalStyle } from 'styled-components';
 import Home from './pages/Home';
 import View from './pages/View';
 import Add from './pages/Add';
 import NotFound from './pages/NotFound';
 
 function App() {
-  const GlobalStyle = createGlobalStyle`
-  body {
-    background: linear-gradient(to top, #86dbff 0, #e0c3fc 100%);
-  }`;
-
   return (
     <>
-      <GlobalStyle />
       <BrowserRouter>
         <Switch>
           <Route path="/weathers/view" component={View} />
