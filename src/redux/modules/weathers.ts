ed6119edc3172b7c5a22c84735c26588f9870c80
kwansikdev <kwansk0424@gmail.com>
2020-04-18 17:37:51 +0900

    feat: Add getWeekendWeather function

--- src/redux/modules/weathers.ts
+++ src/redux/modules/weathers.ts
@@ -1,11 +1,11 @@
 import {
   createReducer,
   createAction,
-  ActionType,
   createAsyncAction,
 } from 'typesafe-actions';
-import { put, call, select, takeLatest, takeEvery } from 'redux-saga/effects';
+import { put, call, select, takeLatest } from 'redux-saga/effects';
 import WeatherServices from '../../services/WeatherServices';
+import { FiveDaysWeatherType } from '../../Type/fiveDaysWeatherType';
 
 const prefix: string = 'weathers/';
 
@@ -75,12 +75,59 @@ function* addFiveDaysWeather({
   payload,
 }: ReturnType<typeof addFiveDaysWeatherSaga>) {
   const fiveDays = yield select(state => state.weathers.fiveDays);
+
+  function getWeekendWeather(data: FiveDaysWeatherType) {
+    const today = new Date();
+    const todayToString = today.toUTCString();
+
+    const todayMilli = Date.parse(todayToString);
+
+    const arrTime = [
+      today.getFullYear(),
+      '0' + (today.getMonth() + 1),
+      today.getDate(),
+    ];
+
+    const todayDate = arrTime.reduce((pre, cur) => pre + '-' + cur);
+
+    // 현재 날짜의 3시간 간격의 데이터 5개
+    const result = data.list.filter(
+      list => list.dt_txt.slice(0, 10) === todayDate,
+    );
+
+    // 가져온 데이터의 현재시간을 밀리초로 변경후 현재시간의 밀리초를 뺀 결과의 배열
+    const resultMili = result.map(list =>
+      Math.abs(Date.parse(list.dt_txt) - todayMilli),
+    );
+
+    // 최소값
+    const minResultMili = resultMili.sort((a, b) => a - b)[0];
+    const minusMinResultMili = resultMili.sort((a, b) => a - b)[0] * -1;
+
+    // 최소값의 밀리초에 현재 밀리초를 더한뒤 현재시간을 가져옴
+    // 현재 시간에서 제일 가까운 시간대의 데이터의 시간
+    const lastTime = new Date(minResultMili + todayMilli);
+    const minusLastTime = new Date(minusMinResultMili + todayMilli);
+
+    // 위에서 가져오 데이터의 시간과 같은 5일치의 데이터에서 맞는 데이터 필터
+    const weekend = data.list.filter(list =>
+      list.dt_txt.slice(11) === lastTime.toTimeString().slice(0, 8)
+        ? list.dt_txt.slice(11) === lastTime.toTimeString().slice(0, 8)
+        : list.dt_txt.slice(11) === minusLastTime.toTimeString().slice(0, 8),
+    );
+
+    return weekend;
+  }
+
   try {
     yield put(actions.request());
     const { data } = yield call(WeatherServices.getFiveDayWeather, payload);
+
+    const weekend = getWeekendWeather(data);
+
     yield put(
       actions.success({
-        fiveDays: [...fiveDays, data],
+        fiveDays: [...fiveDays, weekend],
       }),
     );
   } catch {
