27989c7419c0f9d8eb0ac3e2c6928b544b32f70a
kwansikdev <kwansk0424@gmail.com>
2020-06-16 17:03:49 +0900

    feat: Add 'moment' module & edit currentTime.tsx

--- src/components/Header/CurrentTime.tsx
+++ src/components/Header/CurrentTime.tsx
@@ -1,30 +1,29 @@
 import React from 'react';
+import moment from 'moment';
 import * as S from './Styled';
 import { RouteState } from '../../redux/modules/reducer';
 import { useSelector } from 'react-redux';
 
 export default function CurrentTime() {
   const status = useSelector((state: RouteState) => state.common.status);
-  const today = new Date();
+  // const today = new Date();
 
-  const dataString = today.toLocaleDateString('en', {
-    // year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
+  // const dataString = today.toLocaleDateString('en', {
+  //   // year: 'numeric',
+  //   month: 'long',
+  //   day: 'numeric',
+  // });
 
-  const dayName = today.toLocaleDateString('en', {
-    weekday: 'long',
-  });
+  // const dayName = today.toLocaleDateString('en', {
+  //   weekday: 'long',
+  // });
 
-  const time = today.toTimeString().slice(0, 5);
+  // const time = today.toTimeString().slice(0, 5);
 
   return (
     <S.CurrentTime status={status}>
-      <S.Data>{dataString}t </S.Data>
-      <S.Day>
-        {dayName}, {time}
-      </S.Day>
+      <S.Data>{moment().format('MMM Do YY')},</S.Data>
+      <S.Day>{moment().format('LT')}</S.Day>
     </S.CurrentTime>
   );
 }
